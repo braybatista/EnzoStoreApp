44f1b47ba6937651528fa879fae7b09fc8fe0257
Brayan <brayanbatistaz@gmail.com>
2023-11-27 20:20:12 -0500

    ajustes archivos sql

--- MySQL/prendas_data.sql
+++
@@ -1,7 +0,0 @@
-/*
--- Query: SELECT * FROM bdenzostore.prendas
--- Date: 2023-11-27 12:01
-*/
-INSERT INTO `` (`id`,`nombre`,`colores`,`talla`,`descripcion`,`precio`,`estado`) VALUES (1,'test','','25','test',2000,'test');
-INSERT INTO `` (`id`,`nombre`,`colores`,`talla`,`descripcion`,`precio`,`estado`) VALUES (2,'test2','','25','test2',1000,'test2');
-INSERT INTO `` (`id`,`nombre`,`colores`,`talla`,`descripcion`,`precio`,`estado`) VALUES (3,'test3','','25','test3',50,'test3');
--- MySQL/usuarios_data.sql
+++
@@ -1,6 +0,0 @@
-/*
--- Query: SELECT * FROM bdenzostore.usuarios
--- Date: 2023-11-27 11:59
-*/
-INSERT INTO `` (`id`,`nombres`,`apellidos`,`correo`,`contraseña`,`tipocontacto`,`historial_compras`) VALUES (1,'brayan','batista','test','123','Cliente','test');
-INSERT INTO `` (`id`,`nombres`,`apellidos`,`correo`,`contraseña`,`tipocontacto`,`historial_compras`) VALUES (13,'brayan','batista','test02','123','Cliente',NULL);
